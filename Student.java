8058fc9b02460c97a08e470af06b226246e11663
Mandip <np03cs4a230101@heraldcollege.edu.np>
2025-01-13 10:24:42 +0545

    Adding student file

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    int age;
+    String name;
+public Student(int age,String name){
+    this.age=1;
+    this.name="MAndip";
+}
+public static void main(String[] args){
+
+}
+}
