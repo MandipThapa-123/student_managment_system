ca7fbb6045cd8bf223b7afcb63c1500fa8d94d9d
Mandip <np03cs4a230101@heraldcollege.edu.np>
2025-01-13 10:30:53 +0545

    adding getter and setter

--- Student.java
+++ Student.java
@@ -5,6 +5,18 @@ public Student(int age,String name){
     this.age=1;
     this.name="MAndip";
 }
+public int getAge() {
+    return age;
+}
+public void setAge(int age) {
+    this.age = age;
+}
+public String getName() {
+    return name;
+}
+public void setName(String name) {
+    this.name = name;
+}
 public static void main(String[] args){
 
 }
